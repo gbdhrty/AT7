c57739bed9af5de33d2bef177745e5944c222ca7
Doherty <gabriel.dcarvalho@al.infnet.edu.br>
2024-09-24 20:22:24 -0300

    Adicionar arquivos de projeto.

---
+++ AT7/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using AT7.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AT7.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base (options) { }
+
+        public DbSet<Livro> livros { get; set; }
+    }
+}
---
+++ AT7/Models/Livro.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AT7.Models
+{
+    public class Livro
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+    }
+}
